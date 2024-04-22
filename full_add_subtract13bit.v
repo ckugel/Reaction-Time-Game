5483ecddf0933d02b35ca11ca8ebf637c31427c4
Fjellman <fjellman@iastate.edu>
2024-04-21 22:51:55 -0500

    in theory finished devide by 10 module have not tested it

--- full_add_subtract13bit.v
+++ full_add_subtract13bit.v
@@ -1,4 +1,4 @@
-`include "13Adder.v"
+`include "Adder13.v"
 module full_add_subtract13bit(X, Y, AddSub, Overflow, S);
   input [12:0] X;
   input [12:0] Y;
@@ -8,20 +8,20 @@ module full_add_subtract13bit(X, Y, AddSub, Overflow, S);
 
   wire [12:0] In;
 
-  assign In[0] = AddSub ^ X[0];
-  assign In[1] = AddSub ^ X[1];
-  assign In[2] = AddSub ^ X[2];
-  assign In[3] = AddSub ^ X[3];
-  assign In[4] = AddSub ^ X[4];
-  assign In[5] = AddSub ^ X[5];
-  assign In[6] = AddSub ^ X[6];
-  assign In[7] = AddSub ^ X[7];
-  assign In[8] = AddSub ^ X[8];
-  assign In[9] = AddSub ^ X[9];
-  assign In[10] = AddSub ^ X[10];
-  assign In[11] = AddSub ^ X[11];
-  assign In[12] = AddSub ^ X[12];
+  assign In[0] = AddSub ^ Y[0];
+  assign In[1] = AddSub ^ Y[1];
+  assign In[2] = AddSub ^ Y[2];
+  assign In[3] = AddSub ^ Y[3];
+  assign In[4] = AddSub ^ Y[4];
+  assign In[5] = AddSub ^ Y[5];
+  assign In[6] = AddSub ^ Y[6];
+  assign In[7] = AddSub ^ Y[7];
+  assign In[8] = AddSub ^ Y[8];
+  assign In[9] = AddSub ^ Y[9];
+  assign In[10] = AddSub ^ Y[10];
+  assign In[11] = AddSub ^ Y[11];
+  assign In[12] = AddSub ^ Y[12];
 
-  full_adder_13bit FA (AddSub, X, In, Overflow, S);
+  Adder13 FA (AddSub, X, In, Overflow, S);
 
-  endmodule
+endmodule
