6b7ac360933e944e81d43d1ba9d8861daec97209
ckugel <calebkugel1@gmail.com>
2024-04-22 23:57:41 -0500

    added support files

---
+++ regfile.v
@@ -0,0 +1,14 @@
+module regfile(DATAP, DATAQ, RP, RQ, WA, LD_DATA, WR, CLK);
+	output [3:0] DATAP;
+	output [3:0] DATAQ;
+	input [2:0] RP;
+	input [2:0] RQ;
+	input [2:0] WA;
+	input [3:0] LD_DATA;
+	input WR;
+	input CLK;
+	
+	
+
+
+endmodule
