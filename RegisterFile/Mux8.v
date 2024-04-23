3dc079eb81bbce30076738bf5ba97497fdaf7f4e
ckugel <calebkugel1@gmail.com>
2024-04-23 02:02:40 -0500

    added the register file and it built

--- RegisterFile/Mux8.v
+++ RegisterFile/Mux8.v
@@ -1,18 +1,26 @@
 module Mux8(S2, S1, S0, W7, W6, W5, W4, W3, W2, W1, W0, F);
-	input [3:0] W0;
-	input [3:0] W1;
-	input [3:0] W2;
-	input [3:0] W3;
-	input [3:0] W4;
-	input [3:0] W5;
-	input [3:0] W6;
-	input [3:0] W7;
+	input [12:0] W0;
+	input [12:0] W1;
+	input [12:0] W2;
+	input [12:0] W3;
+	input [12:0] W4;
+	input [12:0] W5;
+	input [12:0] W6;
+	input [12:0] W7;
 	
 	input S2;
 	input S1;
 	input S0;
+
+	wire [12:0] SW1;
+	wire [12:0] SW2;
+	wire [12:0] SW3;
+	wire [12:0] SW4;
+
+	wire [12:0] ST1;
+	wire [12:0] ST2;
 	
-	output [3:0] F;
+	output [12:0] F;
 	
 	assign SW1 = S0 ? W1 : W0;
 	assign SW2 = S0 ? W3 : W2;
