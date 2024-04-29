53ce95c68a817098dda5674c34dbfb580396b7d1
ckugel <calebkugel1@gmail.com>
2024-04-28 21:11:31 -0500

    maybe fix

--- full_add_subtract13bit.v
+++ full_add_subtract13bit.v
@@ -1,32 +1,31 @@
-`ifndef full_add_subtract13bit
-  `define full_add_subtract13bit
+`include "half_adder.v" 
 
-`include "Adder13.v"
-module full_add_subtract13bit(X, Y, AddSub, Overflow, S);
+module full_adder_13bit(X, Y, Cin, Sout, Cout, Overflow);
   input [12:0] X;
   input [12:0] Y;
-  input AddSub;
+  input Cin;
+
   output Overflow;
-  output [12:0] S;
+  output [12:0] Sout;
+  output Cout;
 
-  wire [12:0] In;
+  half_adder adder1 (X[0], Y[0], Cin, Cnext, Sout[0]);
+  half_adder adder2 (X[1], Y[1], Cnext, Cnext, Sout[1]);
+  half_adder adder3 (X[2], Y[2], Cnext, Cnext, Sout[2]);
+  half_adder adder4 (X[3], Y[3], Cnext, Cnext, Sout[3]);
+  half_adder adder5 (X[4], Y[4], Cnext, Cnext, Sout[4]);
+  half_adder adder6 (X[5], Y[5], Cnext, Cnext, Sout[5]);
+  half_adder adder7 (X[6], Y[6], Cnext, Cnext, Sout[6]);
+  half_adder adder8 (X[7], Y[7], Cnext, Cnext, Sout[7]);
+  half_adder adder9 (X[8], Y[8], Cin, Cnext, Sout[8]);
+  half_adder adder10 (X[9], Y[9], Cin, Cnext, Sout[9]);
+  half_adder adder11 (X[10], Y[10], Cnext, Cnext, Sout[10]);
+  half_adder adder12 (X[11], Y[11], Cnext, Cnext1, Sout[11]);
+  half_adder adder13 (X[12], Y[12], Cnext1, Cnext, Sout[12]);
 
-  assign In[0] = AddSub ^ Y[0];
-  assign In[1] = AddSub ^ Y[1];
-  assign In[2] = AddSub ^ Y[2];
-  assign In[3] = AddSub ^ Y[3];
-  assign In[4] = AddSub ^ Y[4];
-  assign In[5] = AddSub ^ Y[5];
-  assign In[6] = AddSub ^ Y[6];
-  assign In[7] = AddSub ^ Y[7];
-  assign In[8] = AddSub ^ Y[8];
-  assign In[9] = AddSub ^ Y[9];
-  assign In[10] = AddSub ^ Y[10];
-  assign In[11] = AddSub ^ Y[11];
-  assign In[12] = AddSub ^ Y[12];
+  assign Overflow = Cnext1 ^ Cnext;
+  assign Cout = Cnext1;
 
-  Adder13 FA (AddSub, X, In, Overflow, S);
+  endmodule
+  
 
-endmodule
-
-`endif
