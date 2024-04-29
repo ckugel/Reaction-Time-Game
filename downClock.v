26547e2dd7fcb1c2af73d5c448d6ff00662ef4ad
ckugel <calebkugel1@gmail.com>
2024-04-28 21:24:51 -0500

    supporting files

---
+++ downClock.v
@@ -0,0 +1,12 @@
+`include "tff.v"
+`include "counterComponent.v"
+`include "equals50k.v"
+
+module downClock(Enable, ClkIn, ClkOut);
+  input Enable;
+  output ClkIn;
+  output ClkOut;
+
+   
+
+  endmodule
