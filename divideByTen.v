6b7bda7abce9f4af86cee43499b8b8dec0995403
ckugel <calebkugel1@gmail.com>
2024-04-30 05:04:51 -0500

    potential fix for divide by ten
    bit shift may have been the wrong direction

--- divideByTen.v
+++ divideByTen.v
@@ -9,8 +9,8 @@ module divideByTen(dividend, quotient);
   
   wire [12:0] result1;
 
-  full_add_subtract13bit FA1 (.X({dividend[3], dividend[4], dividend[5], dividend[6], dividend[7], dividend[8], dividend[9], dividend[10], dividend[11], dividend[12], 1'b0, 1'b0, 1'b0}), .Y({dividend[4], dividend[5], dividend[6], dividend[7], dividend[8], dividend[9], dividend[10], dividend[11], dividend[12], 1'b0, 1'b0, 1'b0, 1'b0}), .AddSub(1'b1), .Overflow(_ignore), .S(result1));
-  full_add_subtract13bit FA2 (.X({dividend[5], dividend[6], dividend[7], dividend[8], dividend[9], dividend[10], dividend[11], dividend[12], 1'b0, 1'b0, 1'b0, 1'b0, 1'b0}), .Y(result1), .AddSub(1'b0), .Overflow(_ignore), .S(quotient));
+  full_add_subtract13bit FA1 (.X({1'b0, 1'b0, 1'b0, dividend[12], dividend[11], dividend[10], dividend[9], dividend[8], dividend[7], dividend[6], dividend[5], dividend[4], dividend[3]}), .Y({1'b0, 1'b0, 1'b0, 1'b0, dividend[12], dividend[11], dividend[10], dividend[9], dividend[8], dividend[7], dividend[6], dividend[5], dividend[4]}), .AddSub(1'b1), .Overflow(_ignore), .S(result1));
+  full_add_subtract13bit FA2 (.X({1'b0, 1'b0, 1'b0, 1'b0, 1'b0, dividend[12], dividend[11], dividend[10], dividend[9], dividend[8], dividend[7], dividend[6], dividend[5]}), .Y(result1), .AddSub(1'b0), .Overflow(_ignore), .S(quotient));
   
 
   endmodule
